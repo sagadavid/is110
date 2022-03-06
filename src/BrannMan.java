807f6d2c66c2ce11cd27aecbc464d25b42ec0ea2
David Saga <epostsaga@gmail.com>
2022-03-07 00:48:13 +0100

    modul347 asided and modul6 added

--- src/BrannMan.java
+++ src/BrannMan.java
@@ -1,12 +1,12 @@
 
 //MODUL-3
+
 public class BrannMan {
     private int brannNo;
 
     public BrannMan() {
         brannNo = 110;
     }
-
     public void ringBrannMan() {
         System.out.println("du ringer på " + brannNo);
     }
